40fcd2e8067d9fb666cfe1e71e8a4e9ce982120a
Cosmin Iordache <iordachecosmin28@gmail.com>
2025-07-21 00:26:02 +0300

    add Eninge Dependency injection to have all dependecies in one place

---
+++ Engine/AV.Engine.DI/ServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using AV.Engine.Core.Interfaces;
+using AV.Engine.Persistence.Persistence;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AV.Engine.DI
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddAVEngineServices(this IServiceCollection services)
+        {
+            //services.AddLogging(builder => builder.AddConsole().AddDebug());
+
+            services.AddSingleton<IEventStore, InMemoryEventStore>();
+            services.AddSingleton<IThreatSimulator, ThreatSimulator>();
+            services.AddTransient<IScanEngine, MockScanEngine>();
+            services.AddSingleton<IAVEngine, AVEngine>();
+
+            return services;
+        }
+    }
+}
